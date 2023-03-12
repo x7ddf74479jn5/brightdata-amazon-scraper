e7c97d88315d5c6f87e208f2f6bc602dc3920721
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2023-03-13 02:19:58 +0900

    ♻️ refactor

--- packages/web/pages/api/activateScraper.ts
+++ packages/web/pages/api/activateScraper.ts
@@ -38,7 +38,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
       search,
       start_eta,
       status: "pending",
-      updateAt: admin.firestore.Timestamp.now(),
+      updateAt: start_eta,
     });
 
     res.status(200).json({
