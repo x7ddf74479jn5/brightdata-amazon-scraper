ecec0a47602cddbfa5f29e42176329426427d33b
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2023-03-18 22:15:36 +0900

    ✨ feat: impl search page
    Delete button and loading spinner

--- packages/web/app/search/[id]/page.tsx
+++ packages/web/app/search/[id]/page.tsx
@@ -1,7 +1,9 @@
-import { doc } from "firebase/firestore";
+import { deleteDoc, doc } from "firebase/firestore";
 import { useDocument } from "react-firebase-hooks/firestore";
 import { Results } from "../../../components/Results";
 import { db } from "../../../firebase";
+import Spinner from "react-spinkit";
+import { useRouter } from "next/navigation";
 
 type Props = {
   params: {
@@ -11,6 +13,18 @@ type Props = {
 
 const SearchPage = ({ params: { id } }: Props) => {
   const [snapshot, loading, error] = useDocument(doc(db, "searches", id));
+  const router = useRouter();
+
+  const handleDelete = () => {
+    deleteDoc(doc(db, "searches", id));
+    router.push("/");
+  };
+
+  const deleteButton = (
+    <button onClick={handleDelete} className="bg-indigo-600 text-white px-4 py-2 rounded-lg">
+      Delete Search
+    </button>
+  );
 
   if (loading) {
     return <h1 className="text-center p-10 animate-pulse text-xl text-indigo-600/50">Loading results...</h1>;
@@ -22,14 +36,25 @@ const SearchPage = ({ params: { id } }: Props) => {
     return (
       <div className="flex flex-col gap-y-5 py-10 items-center justify-between">
         <p className="text-indigo-600 animate-pulse text-center">Scraping results from Amazon...</p>
+        <Spinner
+          style={{
+            height: "100px",
+            width: "100px",
+          }}
+          name="cube-grid"
+          fadeIn="none"
+          color="indigo"
+        />
+
+        {deleteButton}
       </div>
     );
   }
 
   return (
-    <div>
-      <div>
-        <div>
+    <div className="py-5">
+      <div className="flex items-center justify-between mb-7">
+        <div className="flex flex-col md:flex-row gap-x-4">
           <h1 className="font-bold">
             Search results for <span className="text-indigo-600">"{snapshot.data()?.search}"</span>
           </h1>
@@ -37,6 +62,8 @@ const SearchPage = ({ params: { id } }: Props) => {
             {snapshot.data()?.results?.length > 0 && `${snapshot.data()?.results?.length} results found`}
           </p>
         </div>
+
+        {deleteButton}
       </div>
 
       {snapshot.data()?.results?.length > 0 && <Results results={snapshot.data()?.results} />}
